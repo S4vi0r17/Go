85fa48f6ea637772bfefb5587fbd7095ffd44eed
S4vi0r <gbp17@outlook.com>
2024-02-13 18:05:54 -0500

    defer, panic, recover in Go

---
+++ 01-Go/13-defer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// defer: A defer statement defers the execution of a function until the surrounding function returns.
+	// defer fmt.Println("world")
+
+	// fmt.Println("hello")
+
+	// defer fmt.Println("1")
+	// defer fmt.Println("2")
+	// defer fmt.Println("3")
+
+	a := 5
+	defer fmt.Println("deferred print:", a)
+	a = 10
+	fmt.Println("regular print:", a)
+}
