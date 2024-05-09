1492543126b69e9eff3bc84f8b25b6cc967b76d6
S4v!0r <109929385+S4vi0r17@users.noreply.github.com>
2024-05-09 18:14:03 -0500

    Add prueba package with Saludar and Sumar functions

---
+++ testsGo/prueba/something.go
@@ -0,0 +1,13 @@
+package prueba
+
+import "fmt"
+
+func Saludar(nombre string) {
+    fmt.Printf("Hola, %s!\n", nombre)
+}
+
+func Sumar(a, b int) int {
+    return a + b
+}
+
+var Pi float64 = 3.14159
