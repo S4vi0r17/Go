d1c11ee4a7cba379b7b627533758725cf7f34c7f
S4vi0r <gbp17@outlook.com>
2024-05-15 15:12:25 -0500

    chore: Add command line flags for filtering and ordering in main.go

--- gls/main.go
+++ gls/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	
+	// Filter by pattern
+	flagPattern := flag.String("p", "", "filter by pattern")
+	flagAll := flag.Bool("a", false, "show all files")
+	flagNumberRecords := flag.Int("n", 0, "number of records to show")
+
+	// Order flags
+	hasOrderByTime := flag.Bool("t", false, "order by time, oldest first")
+	hasOrderBySize := flag.Bool("s", false, "order by size, smallest first")
+	hasOrderReverse := flag.Bool("r", false, "reverse the order")
+
+	flag.Parse() // parse the flags
+
+	fmt.Println(*flagPattern)
+	fmt.Println(*flagAll)
+	fmt.Println(*flagNumberRecords)
+
+	fmt.Println(*hasOrderByTime)
+	fmt.Println(*hasOrderBySize)
+	fmt.Println(*hasOrderReverse)
 }
