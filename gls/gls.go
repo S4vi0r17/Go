236bcbe58d53e58133cbe8a80ab625fe43a71201
S4vi0r <gbp17@outlook.com>
2024-05-15 15:12:08 -0500

    A

--- gls/gls.go
+++ gls/gls.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 // file types
 const (
 	regularFile int = iota
@@ -22,3 +24,30 @@ const (
 	jpg = ".jpg"
 	gif = ".gif"
 )
+
+type file struct {
+	name      string
+	fileType  int
+	isDir     bool
+	isHidden  bool
+	userName  string
+	groupName string
+	size      int64
+	modTime   time.Time
+	mode      string
+}
+
+type styleFileType struct {
+	icon   string
+	color  string
+	symbol string
+}
+
+var fileTypes = map[int]styleFileType{
+	regularFile:    {icon: "📄"},
+	directoryFile:  {icon: "📁", color: "BLUE", symbol: "/"},
+	executableFile: {icon: "🚀", color: "GREEN", symbol: "*"},
+	compressedFile: {icon: "📦", color: "RED"},
+	imageFile:      {icon: "🖼️", color: "MAGENTA"},
+	linkFile:       {icon: "🔗", color: "CYAN"},
+}
