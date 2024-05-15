5066ec6740c5b3b1b5a65191a1d98a6419fc0033
S4v!0r <109929385+S4vi0r17@users.noreply.github.com>
2024-05-15 08:24:41 -0500

    Add constants for file types and extensions in gls.go

---
+++ gls/gls.go
@@ -0,0 +1,24 @@
+package main
+
+// file types
+const (
+	regularFile int = iota
+	directoryFile
+	executableFile
+	compressedFile
+	imageFile
+	linkFile
+)
+
+// file extension
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
