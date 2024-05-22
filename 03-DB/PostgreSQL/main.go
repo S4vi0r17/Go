b22ed29d4dc592aa9096d7ba6cff70364133bfdd
S4vi0r <gbp17@outlook.com>
2024-05-21 20:10:33 -0500

    Connection to PostgreSQL with Go

--- 03-DB/PostgreSQL/main.go
+++ 03-DB/PostgreSQL/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"PostgreSQL/storage"
 )
 
 func main() {
-	fmt.Println("Go PostgreSQL Tutorial")
+	storage.NewPostgresDB()
 }
---
+++ 03-DB/PostgreSQL/storage/storage.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"database/sql"
+	"log"
+	"sync"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+func NewPostgresDB() {
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("postgres", "postgres://postgres:benites1234@localhost:5432/godb?sslmode=disable")
+		if err != nil {
+			log.Fatalf("Error opening database: %q", err)
+		}
+
+		if err = db.Ping(); err != nil {
+			log.Fatalf("Error pinging database: %q", err)
+		}
+
+		log.Println("Successfully connected!")
+	})
+}
+
+// Return a unique instance of the database
+func Pool() *sql.DB {
+	return db
+}
