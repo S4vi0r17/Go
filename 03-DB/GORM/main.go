bd5a731164336db6bccbf4f9eba9a258fbbb55f1
S4vi0r <gbp17@outlook.com>
2024-07-22 15:17:38 -0500

    chore: Read all records from the database

--- 03-DB/GORM/main.go
+++ 03-DB/GORM/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"GORM/model"
 	"GORM/storage"
+	"fmt"
 )
 
 func main() {
@@ -10,30 +11,16 @@ func main() {
 
 	storage.Pool().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
 
-	// Create
-	observations := "Pepperoni"
-	product1 := model.Product{
-		Name:         "Pizza",
-		Observations: &observations,
-		Price:        12.5,
+	// Read all
+	// var products []model.Product
+	products := make([]model.Product, 0)
+	storage.Pool().Find(&products)
+	for _, product := range products {
+		// fmt.Printf("%d %s %s %f\n", product.ID, product.Name, *product.Observations, product.Price)
+		fmt.Printf("%d %s %f\n", product.ID, product.Name, product.Price)
 	}
-
-	product2 := model.Product{
-		Name:  "Coke",
-		Price: 1.5,
-	}
-
-	product3 := model.Product{
-		Name:         "Fries",
-		Observations: ptrString("Large"),
-		Price:        3.5,
-	}
-
-	storage.Pool().Create(&product1)
-	storage.Pool().Create(&product2)
-	storage.Pool().Create(&product3)
 }
 
-func ptrString(s string) *string {
-	return &s
-}
+// func ptrString(s string) *string {
+// 	return &s
+// }
