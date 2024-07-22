19fb7102cd166f22a5e0dacd1fa5df99833cc300
S4vi0r <gbp17@outlook.com>
2024-07-22 14:30:17 -0500

    chore: Add Product and Invoice models to GORM implementation

---
+++ 03-DB/GORM/model/model.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name         string  `gorm:"size:255;not null"`
+	Observations string  `gorm:"type:text"`
+	Price        float64 `gorm:"not null"`
+	Products     []Product
+}
+
+type InvoiceHeader struct {
+	gorm.Model
+	Client       string `gorm:"size:255;not null"`
+	InvoiceItems []InvoiceItem
+}
+
+type InvoiceItem struct {
+	gorm.Model
+	InvoiceHeaderID uint
+	ProductID       uint
+}
