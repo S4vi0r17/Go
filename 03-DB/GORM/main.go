98cb9267a6994f848e603c5e1facd92480e557d9
S4vi0r <gbp17@outlook.com>
2024-07-22 14:37:18 -0500

    chore: Update GORM implementation to include auto migration for new models

--- 03-DB/GORM/main.go
+++ 03-DB/GORM/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "GORM/storage"
+import (
+	"GORM/model"
+	"GORM/storage"
+)
 
 func main() {
 	storage.NewPostgresDB()
+
+	storage.Pool().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
 }
